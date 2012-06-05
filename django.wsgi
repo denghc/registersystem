f6e2046a6e3937d81a3bb11cc9949d81803cb9cf
denghc <denghc09@gmail.com>
2012-06-05 13:27:51 +0800

    6/5

--- django.wsgi
+++ django.wsgi
@@ -1,7 +1,7 @@
 import os
 import sys
 import django.core.handlers.wsgi
-sys.path.append('E:/DjangoWorkSpace')
-sys.path.append('E:/DjangoWorkSpace/RegisterSystem')
+sys.path.append('/home/devsleague/websites')
+sys.path.append('/home/devsleague/websites/RegisterSystem')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'RegisterSystem.settings'
 application = django.core.handlers.wsgi.WSGIHandler() 
--- settings.py
+++ settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'registersystemdb',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'bandeng',                  # Not used with sqlite3.
-        'HOST': '/var/run/mysql',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD': 'Devsleague.admin',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                     # Set to empty string for default. Not used with sqlite3.
     }
 }
